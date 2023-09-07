e9dda9877160770d989f64ac700acd876e44fde3
JITENDRA PARDHI <jitupardhi7694>
2023-09-07 18:14:16 +0530

    updated

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,25 +1,11 @@
 const express = require('express');
-
 const router = express.Router();
-const {ensureAuthenticated} = require('../helpers/auth-helper');
-const dashboardController = require('../controllers/dashboardController');
-const roles = require('../config/roles.json');
 
-// app Landing page
-router.get(['/', '/dashboard'], ensureAuthenticated, async (req, res, next) => {
-   if ([roles.ADMIN, roles.OWNER].includes(req.user.role_id)) {
-      return dashboardController.adminDashboard(req, res, next);
-   }
-   if ([roles.COUNSELLOR].includes(req.user.role_id)) {
-      return dashboardController.counsellerDashboard(req, res, next);
-   }
-   if ([roles.DOCTOR].includes(req.user.role_id)) {
-      return dashboardController.doctorDashboard(req, res, next);
-   }
-   if ([roles.NURSE].includes(req.user.role_id)) {
-      return dashboardController.nurseDashboard(req, res, next);
-   }
-   return res.send('No dashboard available, please check with the developers.').status(404);
+router.get('/', async (req, res) => {
+    await res.render('home');
+});
+router.get('/about', async (req, res) => {
+    await res.render('product/addToCart');
 });
 
 module.exports = router;
